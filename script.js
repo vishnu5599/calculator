25c00b4db6d08b346ba5b788059d41c2c875a68e
vishnu5599 <108262604+vishnu5599@users.noreply.github.com>
2022-08-11 00:22:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventlistener('click', (e) => {
+
+    });
+ });
+
